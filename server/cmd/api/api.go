a6f1c16c6059a394325a9ada51d457268c0bac84
charlie-bit <554486586@qq.com>
2023-09-02 17:55:40 +0800

    [feat] 用flags控制主程，启用conbra

--- server/cmd/api/api.go
+++ server/cmd/api/api.go
@@ -1,9 +1,57 @@
 package main
 
+import (
+	"chat_socket/server/config"
+	"flag"
+	"fmt"
+	"github.com/charlie-bit/utils/gzlog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
-	
+	var (
+		configFile  string
+		ping        bool
+		showVersion bool
+		grpcPort    int
+	)
+
+	flag.StringVar(&configFile, "f", "config/setting.yml", "config file")
+	flag.BoolVar(&ping, "ping", false, "Ping server.")
+	flag.BoolVar(&showVersion, "version", false, "Print version information.")
+	flag.BoolVar(&showVersion, "v", false, "Print version information.")
+	flag.IntVar(&grpcPort, "grpc_port", 0, "grpc port")
+	flag.Parse()
+
+	// get config
+	err := config.InitConfig(configFile)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+		return
+	}
+
+	gzlog.Init()
+	defer func() {
+		gzlog.Exit()
+	}()
+
+	if err = run(grpcPort); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+		return
+	}
+
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM)
+	<-exit
 }
 
-func run(port int) {
-
+func run(port int) error {
+	if port == 0 {
+		port = config.Cfg.APIServerGrpcPort
+	}
+	return nil
 }
--- server/pkg/command/command_interface.go
+++
@@ -1,6 +0,0 @@
-package command
-
-type ServerCommander interface {
-	AddRunE()       // 新增cobra RunE方法调用
-	Execute() error // cobra函数调用
-}
