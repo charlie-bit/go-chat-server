56738b6f67ed2496b740cfb4efcf3b0acd50f46c
charlie <charlie@89trillion.com>
2023-09-04 18:20:48 +0800

    修复push启动

--- server/internal/rpc_push/register_server.go
+++ server/internal/rpc_push/register_server.go
@@ -2,12 +2,7 @@ package rpc_push
 
 import (
 	"chat_socket/server/config"
-	unrelation "chat_socket/server/model/mongo_table"
-	relation "chat_socket/server/model/table"
 	"chat_socket/server/pkg/kafka"
-	"chat_socket/server/pkg/mongo"
-	"chat_socket/server/pkg/mysql"
-	"chat_socket/server/pkg/redis"
 
 	"google.golang.org/grpc"
 )
@@ -16,7 +11,7 @@ type PushRPCServer struct {
 	hConsumer historyConsumer
 }
 
-func StartTransferServer(s *grpc.Server) {
+func StartPushServer(s *grpc.Server) {
 	var srv PushRPCServer
 	srv.hConsumer = historyConsumer{
 		historyConsumerGroup: kafka.NewMConsumerGroup(
@@ -25,21 +20,5 @@ func StartTransferServer(s *grpc.Server) {
 			config.Cfg.Kafka.MsgToPush.Topic,
 		),
 	}
-	rdb, err := redis.NewRedis()
-	if err != nil {
-		return
-	}
-	srv.hConsumer.rdb = rdb
-	mdb, err := mysql.NewMysqlGormDB()
-	if err != nil {
-		return
-	}
-	_ = mdb.AutoMigrate(&relation.ConversationModel{})
-	mongodb, err := mongo.NewMongo()
-	if err != nil {
-		return
-	}
-	srv.hConsumer.convMInter = relation.NewConversationGorm(mdb)
-	srv.hConsumer.msgMongoInter = unrelation.NewMsgMongoDriver(mongodb.GetDatabase())
 	go srv.hConsumer.historyConsumerGroup.RegisterHandleAndConsumer(srv.hConsumer)
 }
